93abeb91a1d103e450969542392182828063b5d4
patoshim3 <patimaactposhta@gmail.com>
2026-03-03 01:39:23 +0500

    removing

--- supabase/functions/server/kv_store.tsx
+++ supabase/functions/server/kv_store.tsx
@@ -10,6 +10,7 @@ CREATE TABLE kv_store_bbafd5d2 (
 // View at https://supabase.com/dashboard/project/hnrdlmqezbdxjniegxnf/database/tables
 
 // This file provides a simple key-value interface for storing Figma Make data. It should be adequate for most small-scale use cases.
+// @ts-ignore
 import { createClient } from "jsr:@supabase/supabase-js@2.49.8";
 
 const client = () => createClient(
